5f9562685c6e9141aebb3eb96d0aadb6bb08f33f
nahyunji <racoonto@gmail.com>
2021-05-28 01:00:25 +0900

    플레이어가 갈 수 있는 벽 최소,최대 위치

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,10 +8,10 @@ public class Player : MonoBehaviour
 {
     public float speed = 0.1f;
     public Animator animator;
-    new public Collider2D collider2D;
+    new public CircleCollider2D collider2D;
     new public Rigidbody2D rigidbody2D;
     public float jumpForce = 100f;
-    public float wallOffset = 0.001f;
+    public float wallOffset = 0.02f;
 
     public enum StateType
     {
@@ -41,7 +41,7 @@ public class Player : MonoBehaviour
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        collider2D = GetComponent<Collider2D>();
+        collider2D = GetComponent<CircleCollider2D>();
         Application.targetFrameRate = 60;
 
         //minX, maxX값을  인스펙터에서 넣으니깐 제대로 넣지 않을 경우 좌우 벽에 붙어 있는 문제 발생 -> trigger Stage상태 유지.
@@ -49,65 +49,122 @@ public class Player : MonoBehaviour
         // 좌우로 레이를 쏘아서 마지막 벽을 min, max로 할당.
 
         RaycastHit2D rightmostHit = new RaycastHit2D();
-        RaycastHit2D Leftmost = new RaycastHit2D();
-        RaycastHit2D hit;
         Vector2 checkPosition = transform.position;
+
+        //오른쪽 벽 구하기
+        checkPosition.x += 100f; //오른쪽 벽을 많이 넘어서 왼쪽으로 측정.
+
         int count = 0;
-        List<RaycastHit2D> hits = new List<RaycastHit2D>();
-        float wallWidth = 1.01f;
-        while (count++ < 10) // 최대 10회만 검사.
+        rightmostHit = Physics2D.Raycast(checkPosition, Vector2.left, 100f, wallLayer); //가장 오른쪽에 있는 벽.
+        //16
+        while (count++ < 10)
         {
-            hit = Physics2D.Raycast(checkPosition, Vector2.right, 100f, wallLayer);
-            if (hit.transform == null)
-                break;
-            hits.Add(hit);
-            rightmostHit = hit; // 마지막 벽이 2중 벽일때 바로 앞에 벽을 마지막 오른쪽 벽으로 하자.
-            checkPosition.x = hit.point.x + wallWidth; //
-        };
+            checkPosition.x = rightmostHit.point.x - 1.01f; //오른쪽 끝벽에서 안쪽으로 들어온 1.01f 값을 뺌. 안쪽벽의 위치를 구하기 위해서.벽위 두께 고려
+            var hit = Physics2D.Raycast(checkPosition, Vector2.left, 100f, wallLayer);
 
-        if (hits.Count >= 2)
-        {
-            var previousHit = hits[hits.Count - 2];
-            if (rightmostHit.point.x < previousHit.point.x + wallWidth + 0.01f)
+            Debug.Log(rightmostHit.point.x); //15, 14
+            if (hit.transform == null) //hit의 위치값이 없으면 break
             {
-                rightmostHit = previousHit;
+                break;
             }
-        }
+            rightmostHit = hit; //오른쪽 끝 벽은 hit이다.
 
-        count = 0;
-        hits.Clear();
+            //12.99
+            //rightmostHit 13.99
+        }
+        Debug.Log("최종 벽 위치" + rightmostHit.point.x);
+        float wallWidth = 1f;
+        maxX = rightmostHit.point.x - wallWidth - collider2D.radius; //maxX는 오른쪽 끝벽 - 벽 두께 - 캐릭터 반지름
+
+        //왼쪽 벽 구하기
         checkPosition = transform.position;
-        while (count++ < 10) // 최대 10회만 검사.
+        checkPosition.x -= 100;
+        var leftmostHit = Physics2D.Raycast(checkPosition, Vector2.right, 100f, wallLayer); //가장 오른쪽에 있는 벽.
+        // -16
+        while (count++ < 10)
         {
-            hit = Physics2D.Raycast(checkPosition, Vector2.left, 100f, wallLayer);
-            if (hit.transform == null)
-                break;
-            hits.Add(hit);
-            Leftmost = hit;
-            checkPosition.x = hit.point.x - 1.01f; // 1.01은 벽 두께
-        };
+            checkPosition.x = leftmostHit.point.x + 1.01f; //오른쪽 끝벽에서 안쪽으로 들어온 1.01f 값을 뺌. 안쪽벽의 위치를 구하기 위해서.벽위 두께 고려
+            var hit = Physics2D.Raycast(checkPosition, Vector2.right, 100f, wallLayer);
 
-        if (hits.Count >= 2)
-        {
-            var previousHit = hits[hits.Count - 2];
-            if (Leftmost.point.x > previousHit.point.x - wallWidth - 0.01f)
+            Debug.Log(leftmostHit.point.x); // -15, -14
+            if (hit.transform == null) //hit의 위치값이 없으면 break
             {
-                Leftmost = previousHit;
-
-                if (hits.Count >= 3) // 왼쪽은 3중벽이어서 추가 확인
-                {
-                    previousHit = hits[hits.Count - 3];
-                    if (Leftmost.point.x > previousHit.point.x - wallWidth - 0.01f)
-                    {
-                        Leftmost = previousHit;
-                    }
-                }
+                break;
             }
+            leftmostHit = hit; //오른쪽 끝 벽은 hit이다.
+
+            //12.99
+            //rightmostHit 13.99
         }
 
-        float halfSize = collider2D.bounds.size.x * 0.5f + wallOffset;
-        maxX = rightmostHit.point.x - halfSize;
-        minX = Leftmost.point.x + halfSize;
+        //leftmostHit 오른쪽으로 1 이동한 다음에 왼쪽으로 레이를 다시 쏜다. 벽의 두께가 1로 딱 떨어지지 않아서.
+        checkPosition.x += 1;
+
+        var hit1 = Physics2D.Raycast(checkPosition, Vector2.left, 2f, wallLayer);
+        leftmostHit = hit1;
+        Debug.Log("최종 왼쪽 벽 위치" + leftmostHit.point.x);
+        minX = leftmostHit.point.x + collider2D.radius + wallOffset; //maxX는 오른쪽 끝벽 + 캐릭터 반지름 + 약간 띄어줌
+
+        //RaycastHit2D Leftmost = new RaycastHit2D();
+        //RaycastHit2D hit;
+
+        //int count = 0;
+        //List<RaycastHit2D> hits = new List<RaycastHit2D>();
+        //float wallWidth = 1.01f; // 벽의 두께. 다음 벽을 체크
+        //while (count++ < 10) // 최대 10회만 검사.
+        //{
+        //    //Raycast의 형식에 주의
+        //    hit = Physics2D.Raycast(checkPosition, Vector2.right, 100f, wallLayer); //반환된 값이 들어온다.  Raycase(자기위치,오른쪽방향 , 100길이만큼, 벽레이어만)
+        //    if (hit.transform == null) //hit 된게 없으면 반복 종료
+        //        break;
+        //    hits.Add(hit); //hit를 모아놓음
+        //    rightmostHit = hit; // 마지막 벽이 2중 벽일때 바로 앞에 벽을 마지막 오른쪽 벽으로 하자.
+        //    checkPosition.x = hit.point.x + wallWidth; //
+        //};
+
+        //if (hits.Count >= 2)
+        //{
+        //    var previousHit = hits[hits.Count - 2];
+        //    if (rightmostHit.point.x < previousHit.point.x + wallWidth + 0.01f)
+        //    {
+        //        rightmostHit = previousHit;
+        //    }
+        //}
+
+        //count = 0;
+        //hits.Clear();
+        //checkPosition = transform.position;
+        //while (count++ < 10) // 최대 10회만 검사.
+        //{
+        //    hit = Physics2D.Raycast(checkPosition, Vector2.left, 100f, wallLayer);
+        //    if (hit.transform == null)
+        //        break;
+        //    hits.Add(hit);
+        //    Leftmost = hit;
+        //    checkPosition.x = hit.point.x - 1.01f; // 1.01은 벽 두께
+        //};
+
+        //if (hits.Count >= 2)
+        //{
+        //    var previousHit = hits[hits.Count - 2];
+        //    if (Leftmost.point.x > previousHit.point.x - wallWidth - 0.01f)
+        //    {
+        //        Leftmost = previousHit;
+
+        //        if (hits.Count >= 3) // 왼쪽은 3중벽이어서 추가 확인
+        //        {
+        //            previousHit = hits[hits.Count - 3];
+        //            if (Leftmost.point.x > previousHit.point.x - wallWidth - 0.01f)
+        //            {
+        //                Leftmost = previousHit;
+        //            }
+        //        }
+        //    }
+        //}
+
+        //float halfSize = collider2D.bounds.size.x * 0.5f + wallOffset;
+        //maxX = rightmostHit.point.x - halfSize;
+        //minX = Leftmost.point.x + halfSize;
     }
 
     private void Update()
